93b81af8037f7471dc24ddf422d1c81058af58ab
Matheus <matheusyagami147@gmail.com>
2021-09-27 12:55:12 -0300

    criada a rota de criação de fornecedor. Criada a classe de fornecedor para a criação de novos fornecedores

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,9 +1,17 @@
 const router = require('express').Router()
 const tabelaFornecedor = require('./tabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
 router.get('/fornecedores', async (req, res ) => {
     const resultados = await tabelaFornecedor.listar()
     res.json(resultados)
+})
+
+router.post('/fornecedores', async (req, res) => {
+    console.log(req.body)
+    const fornecedor = new Fornecedor(req.body)
+    await fornecedor.criar()
+    res.json(fornecedor)
 })
 
 module.exports = router
--- api/rotas/fornecedores/tabelaFornecedor.js
+++ api/rotas/fornecedores/tabelaFornecedor.js
@@ -3,5 +3,9 @@ const modelo = require('./modeloTabelaFornecedor')
 module.exports = {
     listar() {
         return modelo.findAll()
+    },
+
+    inserir(fornecedor) {
+        return modelo.create(fornecedor)
     }
 }

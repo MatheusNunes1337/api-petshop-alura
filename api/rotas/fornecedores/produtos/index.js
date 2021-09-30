9794d2fa3c4c6127106bee4b8a99122fc56f2a47
Matheus <matheusyagami147@gmail.com>
2021-09-30 14:36:25 -0300

    modificado o req.params.fornecedorId por req.fornecedor.id (criado no middleware de verificação de fornecedor) nas rotas de produtos

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -3,13 +3,13 @@ const tabelaProduto = require('./tabelaProduto')
 const Produto = require('./Produto')
 
 produtoRouter.get('/', async (req, res) => {
-    const produtos = await tabelaProduto.listar(req.params.idFornecedor)
+    const produtos = await tabelaProduto.listar(req.fornecedor.id)
     res.json(produtos)
 })
 
 produtoRouter.post('/', async (req, res, proximo) => {
     try {
-        const idFornecedor = req.params.idFornecedor
+        const idFornecedor = req.fornecedor.id
         const dados = Object.assign({}, req.body, { fornecedor: idFornecedor })
         const produto = new Produto(dados)
         await produto.criar()
@@ -22,7 +22,7 @@ produtoRouter.post('/', async (req, res, proximo) => {
 produtoRouter.delete('/:id', async (req, res) => {
     const dados = {
         id: req.params.id,
-        fornecedor: req.params.idFornecedor
+        fornecedor: req.fornecedor.id
     }
     console.log(dados)
     const produto = new Produto(dados)
